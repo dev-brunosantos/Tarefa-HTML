8d3330d84b04763a03795262cbf1f986f26740b4
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-03 13:30:51 -0300

    Criando o alerta explicativo do funcionamento da aplicação

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const containerTarefas = document.querySelector('.container_tarefas');
 
 function IniciarTarefas() {
     const txt = input.value;
-    if(input.value === '' || txt.trim() === '') return alert('Digite uma tarefa');
+    if (input.value === '' || txt.trim() === '') return alert('Digite uma tarefa');
     tarefas.CriarTarefa(input.value);
     input.value = '';
 }
@@ -12,9 +12,24 @@ function IniciarTarefas() {
 btnSalvar.addEventListener('click', IniciarTarefas);
 
 window.addEventListener('keydown', (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
         IniciarTarefas()
     }
 });
 
 const tarefasCadastradas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+
+const duvidas = document.querySelector('.btn-icone');
+
+duvidas.addEventListener('click', () => {
+    alert(`
+    1. Digite uma tarefa no campo de texto e clique no botão salvar ou pressione a tecla Enter.
+    
+    2. Para excluir uma tarefa, clique no botão 'X'.
+    
+    3. Para concluir uma tarefa, clique e segure por alguns segundos.
+    
+    4. Para desmarcar uma tarefa, clique duas vezes sobre ela.
+    `)
+})
